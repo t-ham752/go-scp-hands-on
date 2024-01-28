60b6f49675f871fa0d9f89387fa14e3ec3873b00
Taiga Hamoro <smalt752@gmail.com>
2024-01-28 16:26:27 +0900

    Add instructions for preparing SSH and Docker containers

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+// 準備
+// - 秘密鍵ファイルを用意する
+// 	 - mkdir .ssh && ssh-keygen -t ed25519 -f .ssh/go-scp
+// - SSHサーバーを用意する
+// 	 - docker-compose build --no-cache && docker-compose up -d
+// - DockerコンテナにSSHでログインする
+// 	 - ssh -i .ssh/go-scp casone@localhost -p 20021
+// - Dockerコンテナにファイルを転送する(scpが使えることを確認する)
+// 	 - scp -i .ssh/go-scp -P 20021 main.go casone@localhost:/home/casone
+
 func main() {
 	// 秘密鍵ファイルの読み込み
 
